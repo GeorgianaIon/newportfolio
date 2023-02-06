9c64f6165a622f001f0e632c53df566cad0cd764
Georgiana <ion.georgiana@hotmail.com>
2023-02-06 10:08:03 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
     MY_SERVICE_ID: process.env.MY_SERVICE_ID,
     MY_USER_ID: process.env.MY_USER_ID,
     MY_TEMPLATE_ID: process.env.MY_TEMPLATE_ID,
-    REACT_APP_SITE_KEY: process.env.REACT_APP_SITE_KEY,
   }
 }
 
